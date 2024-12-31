6a8cdc65c6110ccf03919f90af970bb0a3060110
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2024-12-31 23:02:39 +0900

    Feat: local 로그인 수정
    useMutation으로 수정 및 로그아웃 부분 서버 변동사항 적용

--- src/api/auth/user/handleUserLogin.js
+++ src/api/auth/user/handleUserLogin.js
@@ -1,33 +1,38 @@
+import { useMutation } from "@tanstack/react-query";
 import { setAccessTokenToLocalStorage } from "../../../services/setAccessTokenToLocalStorage.js";
 import stApi from "../../axiosInterceptor.js";
 import { USER_LOGIN } from "../../config.js";
 
-export const handleUserLogin = async (loginID, password) => {
-  try {
-    const response = await stApi.post(USER_LOGIN, {
-      login_id: loginID,
-      password: password,
-    });
+const useUserLogin = () => {
+  return useMutation({
+    mutationFn: async ({ loginId, password }) => {
+      const response = await stApi.post(USER_LOGIN, {
+        login_id: loginId,
+        password: password,
+      });
+      return response.data.success;
+    },
+    onSuccess: (data) => {
+      console.log("로그인 응답 : ", data);
+      const { access_token, user } = data;
+      const { user_id, name, nickname } = user;
 
-    console.log("로그인 응답 : ", response.data);
-    const { access_token, user } = response.data.success;
-    const { user_id, name, nickname } = user;
+      setAccessTokenToLocalStorage(access_token);
+      console.log(`[handleUserLogin] 로그인 성공!`);
 
-    setAccessTokenToLocalStorage(access_token);
-    console.log(`[handleUserLogin] 로그인 성공!`);
-
-    return { user_id, name, nickname };
-  } catch (error) {
-    // TODO : 에러오는거 대로 처리하기
-    if (error.response && error.response.status === 401) {
-      console.error("잘못된 아이디 또는 비밀번호를 입력했습니다.");
-      alert("잘못된 아이디 또는 비밀번호를 입력했습니다.");
-      return null;
-    }
-    console.error("로그인 요청 중 에러가 발생했습니다.:", error);
-    alert(
-      `로그인 요청 중 에러가 발생했습니다. 관리자에게 문의하세요\nError Code: ${error.response.status}\nMessage: ${error.message}`
-    );
-    return null;
-  }
+      return { user_id, name, nickname };
+    },
+    onError: (error) => {
+      if (error.response) {
+        alert(`${error.response.status} : ${error.response.data.error.reason}`);
+      } else {
+        console.error("로그인 요청 중 에러가 발생했습니다.:", error);
+        alert(
+          `로그인 요청 중 에러가 발생했습니다. 관리자에게 문의하세요\nError Code: ${error.response?.status}\nMessage: ${error.message}`
+        );
+      }
+    },
+  });
 };
+
+export default useUserLogin;
